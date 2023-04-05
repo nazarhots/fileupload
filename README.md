1136fcff91636745246c72ac72c3bd02b9b44480
Nazar Hots <gotsjob@gmail.com>
2023-04-05 16:54:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+#Flask File Upload Web Application
+This is a simple Flask web application that allows users to upload files and download them back. The uploaded file is stored in the **'static/files'** folder. If a file with the same name already exists, it will be renamed with a unique ID to avoid overwriting.
+
+##Prerequisites
+Before running the application, make sure you have the following software installed on your machine:
+
+- *Python (3.6 or higher)*
+- *Flask (2.0.1 or higher)*
+<br>
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/nazarhots/fileupload.git
+```
+2. Install the requirements:
+```bash
+pip install -r requirements.txt
+```
+3. Start the application by running the following command:
+```bash   
+ python3 app.py
+ ```
+<br>
+
+ ## Usage
+- Click the "Upload File" button to upload a file.
+
+- The application will display information about the uploaded file, including its size, upload time, and upload speed.
+
+- Click the "Download" button to download the uploaded file.
---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+import time
+import uuid
+
+from flask import Flask, render_template, request, send_from_directory, url_for
+
+
+UPLOAD_FOLDER = 'static/files'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    """Handles file uploads and returns the download.html template."""
+    start = time.time()
+    file = request.files['file']
+    filename = file.filename
+    
+    if os.path.exists(os.path.join(UPLOAD_FOLDER, filename)):
+        name, ext = os.path.splitext(filename)
+        filename = f"{name}_{uuid.uuid4().hex[:5]}{ext}"
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    execution_duration = time.time() - start
+    file_size_b = os.path.getsize(os.path.join(UPLOAD_FOLDER, filename))
+    upload_speed = file_size_b / (execution_duration * 1000000)
+    file_size_mb = file_size_b / 1048576
+    download_url = url_for('download', filename=filename)
+
+    return render_template('download.html', duration=execution_duration, upload_speed=upload_speed, filename=filename,
+                           download_url=download_url, file_size=file_size_mb)
+
+
+@app.route('/download/<filename>')
+def download(filename: str):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
